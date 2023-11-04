304a935ddca77bc93f230b8527729e3422ef6cff
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2023-11-04 14:07:42 +0700

    Create Structs.cpp

---
+++ Structs.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+  int age;
+  string FirstName;
+  string LastName;
+  int standard;
+};
+
+int main() {
+  Student st;
+    
+  cin >> st.age >> st.FirstName >> st.LastName >> st.standard;
+  cout << st.age << " " << st.FirstName << " " << st.LastName << " " << st.standard;
+  
+  return 0;
+}
